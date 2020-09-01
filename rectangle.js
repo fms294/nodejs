4274313ec026e1f90939a6166d3cd469c5ef345d
fanta <fms294@nyu.edu>
2020-09-01 16:07:56 -0400

    Node callback and Error Handling

---
+++ rectangle.js
@@ -0,0 +1,15 @@
+module.exports = (x, y, callback) => {
+    if(x <= 0 || y <= 0){
+        callback(new Error(`Rectangle dimension must be greater than zero. Received:${x}, ${y}`))
+    }else{
+        setTimeout(() =>
+            callback(null, {
+
+                perimeter:(x, y) => 2 * (x+y),
+                area : (x, y) => x*y
+            }),
+            2000
+        )
+    }
+}
+
